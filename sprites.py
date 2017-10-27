d965f9f102aa16ef5ca096f89a111a900ff09eb6
Rav Chandra <rav@elysium.co.nz>
2017-10-28 00:56:03 +1300

    make Mario transparent

--- sprites.py
+++ sprites.py
@@ -6,7 +6,7 @@ class sprites():
         self.CharacterSpritesheet = sprite.spritesheet('img/characters.gif',16)
         self.TilesSpritesheet = sprite.spritesheet('img/tiles.png',16)
         #sprites
-        self.mario = self.CharacterSpritesheet.image_at((276, 44, 16, 16),4)
+        self.mario = self.CharacterSpritesheet.image_at((276, 44, 16, 16),4, colorkey=-1)
         self.sky = self.TilesSpritesheet.image_at((48, 368, 16, 16),2)
         self.bricks = self.TilesSpritesheet.image_at((16,0,16,16),2)
         self.ground = self.TilesSpritesheet.image_at((0,0,16,16),2)
