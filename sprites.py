7f5a76c1fc8791c0267ee262e9cf4f30e7a8a61c
Marius <marius_matthaeus.bauer@student.reutlingen-university.de>
2017-11-21 23:38:03 +0100

    outsourced spriteInformations in json files

--- sprites.py
+++ sprites.py
@@ -3,46 +3,47 @@ import pygame
 from pygame.color import Color
 from Sprite import Sprite
 from Animation import Animation
-
+import json
+import pprint
 
 class Sprites():
     def __init__(self):
-        self.CharacterSpritesheet = Spritesheet('img/characters.gif',16)
-        self.TilesSpritesheet = Spritesheet('img/tiles.png',16)
+        self.characterSprites = self.loadSprites("./sprites/CharacterSprites.json","character")
+        self.animations = self.loadSprites("./sprites/AnimationSprites.json","animation")
+        self.backgroundSprites = self.loadSprites("./sprites/BackgroundSprites.json","background")
 
-        self.characterSprites = {
-            "mario_idle": Sprite(self.CharacterSpritesheet.image_at(276, 44,2, colorkey=-1,ignoreTileSize=True),True),
-            "mario_run1": Sprite(self.CharacterSpritesheet.image_at(290, 44,2, colorkey=-1,ignoreTileSize=True),True),
-            "mario_run2": Sprite(self.CharacterSpritesheet.image_at(304, 44,2, colorkey=-1,ignoreTileSize=True),True),
-            "mario_run3": Sprite(self.CharacterSpritesheet.image_at(321, 44,2, colorkey=-1,ignoreTileSize=True),True),
-            "mario_break": Sprite(self.CharacterSpritesheet.image_at(337, 44,2, colorkey=-1,ignoreTileSize=True),True),
-            "mario_jump": Sprite(self.CharacterSpritesheet.image_at(355, 44,2, colorkey=-1,ignoreTileSize=True),True)
-        }
+    def loadSprites(self,url, type):
+        with open(url) as jsonData:
+            data = json.load(jsonData)
+            mySpritesheet = Spritesheet(data['spriteSheetURL'],16)
+            dic = {}
+            if(type is "background"):
+                for sprite in data['sprites']:
+                    try: 
+                        colorkey = sprite['colorKey']
+                    except KeyError:
+                        colorkey = None
+                    dic[sprite['name']] = Sprite(mySpritesheet.image_at(sprite['x'],sprite['y'],sprite['scalefactor'],colorkey),sprite['collision'])
+                return dic
+            elif type is "animation":
+                images = []
+                for sprite in data['sprites']:
+                    for image in sprite['images']:
+                        images.append(mySpritesheet.image_at(image['x'],image['y'],image['scale']))
+                    dic[sprite['name']] = Sprite(None,True,animation = Animation(images))
+                return dic
+            elif type is "character":
+                for sprite in data['sprites']:
+                    try: 
+                        colorkey = sprite['colorKey']
+                    except KeyError:
+                        colorkey = None
+                    dic[sprite['name']] = Sprite(mySpritesheet.image_at(sprite['x'],sprite['y'],sprite['scalefactor'],colorkey,True),sprite['collision'])
+                return dic
 
-        self.animations = {
-            "randomBox": Sprite(None,True,animation = Animation([self.TilesSpritesheet.image_at(24,0,2),self.TilesSpritesheet.image_at(25,0,2),self.TilesSpritesheet.image_at(26,0,2)]))
-        }
 
-        self.backgroundSprites = {
-            "sky": Sprite(self.TilesSpritesheet.image_at(3,23,2),False),
-            "bricks": Sprite(self.TilesSpritesheet.image_at(1,0,2),True),
-            "ground": Sprite(self.TilesSpritesheet.image_at(0,0,2),True),
-           
-            "pipeL": Sprite(self.TilesSpritesheet.image_at(0,10,2),True),
-            "pipeR": Sprite(self.TilesSpritesheet.image_at(1,10,2,colorkey=Color(0,0,0)),True),
-            "pipe2L": Sprite(self.TilesSpritesheet.image_at(0,11,2,colorkey=Color(0,0,0)),True),
-            "pipe2R": Sprite(self.TilesSpritesheet.image_at(1,11,2,colorkey=Color(0,0,0)),True),
-           
-            "cloud1_1": Sprite(self.TilesSpritesheet.image_at(0,20,2,colorkey=Color(0,0,0)),False),
-            "cloud1_2": Sprite(self.TilesSpritesheet.image_at(1,20,2,colorkey=Color(0,0,0)),False),
-            "cloud1_3": Sprite(self.TilesSpritesheet.image_at(2,20,2,colorkey=Color(0,0,0)),False),
-            "cloud2_1": Sprite(self.TilesSpritesheet.image_at(0,21,2,colorkey=Color(0,0,0)),False),
-            "cloud2_2": Sprite(self.TilesSpritesheet.image_at(1,21,2,colorkey=Color(0,0,0)),False),
-            "cloud2_3": Sprite(self.TilesSpritesheet.image_at(2,21,2,colorkey=Color(0,0,0)),False),
-           
-            "bush_1": Sprite(self.TilesSpritesheet.image_at(11,11,2,colorkey=Color(0,0,0)),False),
-            "bush_2": Sprite(self.TilesSpritesheet.image_at(12,11,2,colorkey=Color(0,0,0)),False),
-            "bush_3": Sprite(self.TilesSpritesheet.image_at(13,11,2,colorkey=Color(0,0,0)),False),
-        }
+
+            
+
 
 
