3f5e554a48456657d09b7e0661c5959998e6d3ae
Marius <marius_matthaeus.bauer@student.reutlingen-university.de>
2017-12-03 00:34:43 +0100

    fixed bug where mario wouldnt change the direction

--- input.py
+++ input.py
@@ -1,42 +1,51 @@
 import pygame
 from pygame.locals import *
-from Level import Level
 import sys
 
-def checkForInput(mario,level):
-    events = pygame.event.get()
-    for event in events:
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == KEYDOWN:
-            if event.key == pygame.K_LEFT and event.key != pygame.K_RIGHT:
-                mario.traits['goTrait'].direction = -1
-            elif event.key == pygame.K_RIGHT and event.key != pygame.K_LEFT:
-                mario.traits['goTrait'].direction =  1
-            if event.key == K_LSHIFT:
-                mario.traits['goTrait'].boost = True
-            if event.key == K_SPACE:
-                mario.traits['jumpTrait'].jump = True
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
-                mario.traits['goTrait'].direction =  0
-            if event.key == pygame.K_LSHIFT:
-                mario.traits['goTrait'].boost = False
-            if event.key == K_SPACE:
-                mario.traits['jumpTrait'].jump = False
- 
-def moveMarioWithMouse(mario):
-    mouseX = pygame.mouse.get_pos()[0]/32
-    mouseY = pygame.mouse.get_pos()[1]/32
-    if pygame.mouse.get_pressed()[2]:
-        mario.camera.pos.x = -mouseX
-    if pygame.mouse.get_pressed()[0]:
-        mario.vel.x = 0
-        mario.vel.y = 0       
-        print(mario.pos.x,mario.pos.y)
-        mario.pos.x = mouseX-mario.camera.pos.x
-        mario.pos.y = mouseY 
+class Input():
+    def __init__(self,entity):
+        self.clicked = False
+        self.mouseX = 0
+        self.mouseY = 0
+        self.entity = entity
+    
+    def checkForInput(self):
+        keys = pygame.key.get_pressed()
+        if(keys[K_LEFT]):
+            self.entity.traits['goTrait'].direction = -1
+        if(keys[K_RIGHT]):
+            self.entity.traits['goTrait'].direction =  1
+        if(not(keys[K_LEFT] or keys[K_RIGHT])):
+            self.entity.traits['goTrait'].direction =  0
         
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LSHIFT:
+                    self.entity.traits['goTrait'].boost = False
+                if event.key == K_SPACE:
+                    self.entity.traits['jumpTrait'].jump = False
+            if event.type == KEYDOWN:
+                if event.key == K_LSHIFT:
+                    self.entity.traits['goTrait'].boost = True
+                if event.key == K_SPACE:
+                   self.entity.traits['jumpTrait'].jump = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.clicked = True
+                self.mouseX = pygame.mouse.get_pos()[0]/32
+                self.mouseY = pygame.mouse.get_pos()[1]/32
+            if event.type == pygame.MOUSEMOTION and self.clicked:
+                self.mouseX = pygame.mouse.get_pos()[0]/32
+                self.mouseY = pygame.mouse.get_pos()[1]/32
+                self.entity.vel.x = 0
+                self.entity.vel.y = 0       
+                self.entity.pos.x = self.mouseX-self.entity.camera.pos.x
+                self.entity.pos.y = self.mouseY 
+            if event.type == pygame.MOUSEBUTTONUP:
+                self.clicked = False
+
 
     
