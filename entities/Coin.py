d83aecf6832bd97fd275766fc7cb0f8e5610bdc3
marius bauer <mbauer@science-computing.de>
2017-12-14 15:34:35 +0100

    every coin instance has now his own animation copy

--- entities/Coin.py
+++ entities/Coin.py
@@ -1,14 +1,14 @@
 from entities.EntityBase import EntityBase
 from classes.Animation import Animation
 import pygame
-from copy import deepcopy
+from copy import copy
 
 class Coin(EntityBase):
     def __init__(self,screen,spriteCollection,x,y,gravity = 0):
         super(Coin,self).__init__(x,y,gravity)
         self.screen = screen
         self.spriteCollection = spriteCollection
-        self.animation = self.spriteCollection.get('coin').animation
+        self.animation = copy(self.spriteCollection.get('coin').animation)
 
     def update(self,cam):
         if(self.alive):
