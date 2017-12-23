0a65dd4abd8ad1e3c54ed91bdfb964b0c702608d
Marius <marius_matthaeus.bauer@student.reutlingen-university.de>
2017-12-23 15:42:55 +0100

    refactored RandomBox Animation

--- entities/RandomBox.py
+++ entities/RandomBox.py
@@ -17,22 +17,15 @@ class RandomBox(EntityBase):
 
     def update(self,cam):
         if(self.alive and self.triggered == False):
-            self.animation.update()
-            self.screen.blit(self.spriteCollection.get("sky").image,(self.rect.x+cam.pos.x*32,self.rect.y))
-            self.screen.blit(self.animation.image,(self.rect.x+cam.pos.x*32,self.rect.y-1))
+            self.animation.update()      
         else:
             self.animation.image = self.spriteCollection.get("empty").image
             if(self.time < self.maxTime):
                 self.time +=1
                 self.rect.y -= self.vel
-                self.screen.blit(self.spriteCollection.get("sky").image,(self.rect.x+cam.pos.x*32,self.rect.y+2))
-                self.screen.blit(self.animation.image,(self.rect.x+cam.pos.x*32,self.rect.y-1))
             else:
                 if(self.time < self.maxTime*2):
                     self.time+=1
                     self.rect.y += self.vel
-                    self.screen.blit(self.spriteCollection.get("sky").image,(self.rect.x+cam.pos.x*32,self.rect.y+2))
-                    self.screen.blit(self.animation.image,(self.rect.x+cam.pos.x*32,self.rect.y-1))
-                else:
-                    self.screen.blit(self.spriteCollection.get("sky").image,(self.rect.x+cam.pos.x*32,self.rect.y))
-                    self.screen.blit(self.animation.image,(self.rect.x+cam.pos.x*32,self.rect.y-1))
+        self.screen.blit(self.spriteCollection.get("sky").image,(self.rect.x+cam.pos.x*32,self.rect.y+2))
+        self.screen.blit(self.animation.image,(self.rect.x+cam.pos.x*32,self.rect.y-1))
