b1e8635f32da817345f0f6bd3dfa6373e17e4baa
Marius <marius_matthaeus.bauer@student.reutlingen-university.de>
2018-10-07 11:00:32 +0200

    added windows standalone build

---
+++ compile.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+import py2exe, glob
+
+setup(
+    #this is the file that is run when you start the game from the command line.  
+    console=['main.py'],
+    
+    #data files - these are the non-python files, like images and sounds
+    data_files = [
+      ("sprites", glob.glob("sprites\\*.json")),
+      ("sfx", glob.glob("sfx\\*.ogg")+glob.glob("sfx\\*.wav")) ,
+      ("levels", glob.glob("levels\\*.json")),
+	  ("img", glob.glob("img\\*.gif")+glob.glob("img\\*.png")),
+	  ('', ['settings.json'])
+    ]
+)
