1f8fa22b1ffd7d21a4c0badf6f826f99da075c29
Finn <25953384+Grewoss@users.noreply.github.com>
2018-10-07 20:42:59 +0200

    Increased readability inside Input.py and split up the huge checkForInput function.

--- classes/Input.py
+++ classes/Input.py
@@ -1,8 +1,6 @@
 import pygame
 from pygame.locals import *
 import sys
-from random import randint
-
 
 
 class Input():
@@ -12,44 +10,53 @@ class Input():
         self.entity = entity
 
     def checkForInput(self):
-        keys = pygame.key.get_pressed()
-        if(keys[K_LEFT] and not keys[K_RIGHT]):
+        self.checkForKeyboardInput()
+        self.checkForMouseInput()
+        self.checkForQuitAndRestartInputEvents()
+
+    def checkForKeyboardInput(self):
+        pressedKeys = pygame.key.get_pressed()
+        if(pressedKeys[K_LEFT] and not pressedKeys[K_RIGHT]):
             self.entity.traits['goTrait'].direction = -1
-        elif(keys[K_RIGHT] and not keys[K_LEFT]):
+        elif(pressedKeys[K_RIGHT] and not pressedKeys[K_LEFT]):
             self.entity.traits['goTrait'].direction = 1
         else:
             self.entity.traits['goTrait'].direction = 0
-        if(keys[K_SPACE]):
+        if(pressedKeys[K_SPACE]):
             self.entity.traits['jumpTrait'].start()
         else:
             self.entity.gravity = 1.25
-        if(keys[K_LSHIFT]):
+        if(pressedKeys[K_LSHIFT]):
             self.entity.traits['goTrait'].boost = True
         else:
             self.entity.traits['goTrait'].boost = False
 
-        mouseX = pygame.mouse.get_pos()[0]
-        mouseY = pygame.mouse.get_pos()[1]
-        if pygame.mouse.get_pressed()[2]:
+    def checkForMouseInput(self):
+        mouseX, mouseY = pygame.mouse.get_pos()
+        if self.isRightMouseButtonPressed():
             self.entity.levelObj.addKoopa(
                 mouseY / 32, mouseX / 32 - self.entity.camera.pos.x)
             self.entity.levelObj.addGoomba(
                 mouseY / 32, mouseX / 32 - self.entity.camera.pos.x)
-        if pygame.mouse.get_pressed()[0]:
+        if self.isLeftMouseButtonPressed():
             self.entity.levelObj.addCoin(
                 mouseX / 32 - self.entity.camera.pos.x, mouseY / 32)
-            #self.entity.vel.x = 0
-            #self.entity.vel.y = 0
-            #self.entity.rect.x = mouseX-self.entity.camera.pos.x*32
-            #self.entity.rect.y = mouseY
-            # print(self.entity.getPosIndex().x,self.entity.getPosIndex().y)
 
+    def checkForQuitAndRestartInputEvents(self):
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            if self.checkIfRestartEvent(event):
                 self.entity.restart = True
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_F5:
-                self.entity.restart = True
+
+    def isLeftMouseButtonPressed(self):
+        return pygame.mouse.get_pressed()[0]
+
+    def isRightMouseButtonPressed(self):
+        return pygame.mouse.get_pressed()[2]
+
+    def checkIfRestartEvent(self, event):
+        return (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE) or 
+        (event.type == pygame.KEYDOWN and event.key == pygame.K_F5)
